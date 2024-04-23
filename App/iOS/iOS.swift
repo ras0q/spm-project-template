c48633820c1c7d0aa685c3f3f24c3eb98d0bf4b2
ras0q <ras0q@users.noreply.github.com>
2024-04-24 02:06:52 +0900

    :sparkles: complete template

---
+++ App/iOS/iOS.swift
@@ -0,0 +1,11 @@
+import Feature
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sources/Feature/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+public struct ContentView: View {
+    public init() {}
+
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
